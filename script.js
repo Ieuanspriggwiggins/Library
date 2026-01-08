b2341b0c0412cff15a30dfd821b3baa9679d2b8c
Ieuan Sprigg-Wiggins <ieuanspriggwiggins@gmail.com>
2026-01-08 14:39:04 +0000

    Add comments

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let booksArray = [];
 
+/**
+ * Book constructor for creating a book.
+ * @param {string} title 
+ * @param {string} author 
+ * @param {number} numberOfPages 
+ * @param {number} currentPage 
+ * @param {boolean} read 
+ */
 function Book(title, author, numberOfPages, currentPage, read = false){
     this.title = title;
     this.author = author;
@@ -8,10 +16,22 @@ function Book(title, author, numberOfPages, currentPage, read = false){
     this.read = read;
 }
 
+/**
+ * Calculates what percentage of the total book has been read.
+ * @returns A percentage of the pages total read.
+ */
 Book.prototype.getPercentageRead = function(){
     return (this.currentPage / this.numberOfPages) * 100;
 }
 
+/**
+ * Creates a book object instance and adds it to the book array.
+ * @param {string} title 
+ * @param {string} author 
+ * @param {number} numberOfPages 
+ * @param {number} currentPage 
+ * @param {boolean} read 
+ */
 function createBook(title, author, numberOfPages, currentPage, read = false){
     const book = new Book(title, author, numberOfPages, currentPage, read);
     booksArray.push(book);
